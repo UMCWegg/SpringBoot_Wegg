a5f256a53d84f06ee42cb918a913d49fd8507b91
goeka <goeka1337@gmail.com>
2025-01-12 18:17:58 +0900

    FEAT: [#11]게시물등록, 댓글등록, 댓글삭제, 이모지등록, 이모지삭제, 해당게시물 모든댓글조회 api구현완료. 아직 3개남음

---
+++ src/main/java/umc/wegg/repository/CommentRepository.java
@@ -0,0 +1,19 @@
+package umc.wegg.repository;
+
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import umc.wegg.domain.Comment;
+import umc.wegg.domain.Post;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    // 커스텀 메서드 예: 특정 Post의 모든 댓글 가져오기
+    List<Comment> findByPostId(Long postId);
+
+    List<Comment> findByPost(Post post); //페이징 적용안함. 아직
+
+}
+
---
+++ src/main/java/umc/wegg/repository/PlanRepository.java
@@ -0,0 +1,7 @@
+package umc.wegg.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import umc.wegg.domain.Plan;
+
+public interface PlanRepository extends JpaRepository<Plan, Long> {
+}
