31480c0085f9d33c592d52b0c8193d0fd1a6960d
2ivii <163484088+2ivii@users.noreply.github.com>
2025-01-19 13:02:13 +0900

    FIX: [#6] validation 설정,,

--- src/main/java/umc/wegg/dto/TodoRequestDTO.java
+++ src/main/java/umc/wegg/dto/TodoRequestDTO.java
@@ -1,13 +1,19 @@
 package umc.wegg.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import lombok.Setter;
 import umc.wegg.domain.enums.TodoListStatus;
 
 public class TodoRequestDTO {
     @Getter
+    @Setter
     public static class AddDTO {
+        @NotNull
         private Long userId;
+        @NotNull
         private TodoListStatus status;
+        @NotNull
         private String content;
     }
 
