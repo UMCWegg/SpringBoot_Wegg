4f822cfd8f20ab9fd4309dd6fac1ba8f94332789
seoyoungbyun <qustjdud2181@gmail.com>
2025-01-21 09:04:11 +0900

    FEAT: [#8]SecurityConfig 설정

--- src/main/java/umc/wegg/config/security/SecurityConfig.java
+++ src/main/java/umc/wegg/config/security/SecurityConfig.java
@@ -1,33 +1,80 @@
 package umc.wegg.config.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    @Order(0)
+    public SecurityFilterChain filterChain(HttpSecurity http, CustomOAuth2SuccessHandler customOAuth2SuccessHandler, CustomOAuth2UserService customOAuth2UserService) throws Exception {
         http
-                .csrf((csrf) -> csrf
-                        .ignoringRequestMatchers("/users/**") // REST API 경로에서는 CSRF 비활성화
+                .csrf(csrf -> csrf.disable())
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(login -> login.disable()) // 폼로그인 비허용
+                .oauth2Login(oauth2 -> oauth2
+                        .userInfoEndpoint(userInfo -> userInfo.userService(customOAuth2UserService))
+                        .successHandler(customOAuth2SuccessHandler)
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/users/logout") // 로그아웃 요청 URL
+                        .logoutSuccessHandler((request, response, authentication) -> {
+                            response.setContentType("application/json");
+                            response.setCharacterEncoding("UTF-8");
+                            response.getWriter().write("{\"success\": true}");
+                        })
+                        .invalidateHttpSession(true) // 세션 무효화
+                        .deleteCookies("JSESSIONID") // JSESSIONID 쿠키 삭제
                 )
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(
-                                "/users/**",
-                                "/swagger-ui/**", // Swagger UI 관련 URL
-                                "/v3/api-docs/**"  // OpenAPI 명세 URL
-                        ).permitAll() // 로그인 없이 접근 허용
-                        .anyRequest().authenticated()
-                );
+                        .anyRequest().permitAll())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
+                .securityContext(securityContext -> new HttpSessionSecurityContextRepository());
 
         return http.build();
+    }
+
+    @Bean
+    @Order(1)
+    public SecurityFilterChain addCustomFilters(HttpSecurity http,
+                                                LoginProcessingFilter loginProcessingFilter) throws Exception {
+        http
+                .addFilterBefore(loginProcessingFilter, UsernamePasswordAuthenticationFilter.class); // 로그인 필터 등록
+
+        return http.build();
+    }
+
+    @Bean
+    public LoginProcessingFilter loginProcessingFilter(AuthenticationManager authenticationManager) {
+        LoginProcessingFilter loginProcessingFilter = new LoginProcessingFilter();
+        loginProcessingFilter.setAuthenticationManager(authenticationManager);
+        loginProcessingFilter.setAuthenticationSuccessHandler(new CustomAuthenticationSuccessHandler());
+        loginProcessingFilter.setAuthenticationFailureHandler(new CustomAuthenticationFailureHandler());
+        return loginProcessingFilter;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationProvider authenticationProvider) {
+        return new ProviderManager(authenticationProvider);
     }
 
     @Bean
