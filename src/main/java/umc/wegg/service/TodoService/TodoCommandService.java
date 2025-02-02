3279a6eb82fb95d72bb767067363e88291effc15
2ivii <163484088+2ivii@users.noreply.github.com>
2025-02-02 19:23:52 +0900

    Feat: [#6] 투두 삭제 및 리스트 조회 api

--- src/main/java/umc/wegg/service/TodoService/TodoCommandService.java
+++ src/main/java/umc/wegg/service/TodoService/TodoCommandService.java
@@ -3,8 +3,12 @@ package umc.wegg.service.TodoService;
 import umc.wegg.domain.TodoList;
 import umc.wegg.dto.TodoRequestDTO;
 
+import java.util.List;
+
 public interface TodoCommandService {
     TodoList addTodo(TodoRequestDTO.AddDTO request);
     TodoList updateTodo(Long todoId, TodoRequestDTO.UpdateDTO request); // updateTodo 추가
     double getAchievementRate(Long userId);
+    List<TodoList> getUserTodos(Long userId);
+    void deleteTodo(Long todoId);
 }
