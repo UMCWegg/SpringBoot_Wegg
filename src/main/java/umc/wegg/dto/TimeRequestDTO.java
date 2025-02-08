706e38253379c49f7622381083fc4c2d29a9d460
seungjae708 <choesj365@gmail.com>
2025-02-09 02:52:43 +0900

    FEAT: [#9] 홈화면 및 계란판 세션 로그인 적용(세션을 통한 유저 아이디 탐색)

--- src/main/java/umc/wegg/dto/TimeRequestDTO.java
+++ src/main/java/umc/wegg/dto/TimeRequestDTO.java
@@ -19,7 +19,7 @@ public class TimeRequestDTO {  // 시간 기록
     @Schema(description = "지속 시간 (분 단위)", example = "90")
     private Integer duration;
 
-    @NotNull(message = "사용자 ID는 필수 입력 항목입니다.")
-    @Schema(description = "사용자 ID", example = "1")
-    private Long userId; // 사용자 ID
+//    @NotNull(message = "사용자 ID는 필수 입력 항목입니다.")
+//    @Schema(description = "사용자 ID", example = "1")
+//    private Long userId; // 사용자 ID
 }
