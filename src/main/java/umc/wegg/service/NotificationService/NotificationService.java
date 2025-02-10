d108936fa5de72a69963748af3b1924a7b1b804a
seungjae708 <64647590+seungjae708@users.noreply.github.com>
2025-02-10 12:49:09 +0900

    Feat: [#49] post알림 구현

--- src/main/java/umc/wegg/service/NotificationService/NotificationService.java
+++ src/main/java/umc/wegg/service/NotificationService/NotificationService.java
@@ -10,11 +10,12 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface NotificationService {
-    SseEmitter subscribe(Long memberId, String lastEventId);
+    SseEmitter subscribe(Long userId, String lastEventId);
     void send(User receiver, NotificationType notificationType, String content, String url);
     void sendNotificationToPostOwner(User postOwner, Long postId, String message, String notificationType);
     void sendNotificationToEggOwner(User planUser, String content, String notificationType);
     void scheduleNotification(User user, NotificationType type, LocalDateTime notificationTime, String content, String url);
     List<Notification> getUserNotifications(Long userId);
     Notification readNotification(Long notificationId, NotificationRequestDTO.ReadDTO request);
+    void sendNotificationToFollower(User follower, Long postId, String message, String notificationType);
 }
