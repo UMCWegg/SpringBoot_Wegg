6cf1ec2739de1f602e00c7325a3c1f14ebd09733
seoyoungbyun <qustjdud2181@gmail.com>
2025-02-13 22:53:19 +0900

    FIX: [#72] 장소 리스트 조회 GET으로 변경

--- src/main/java/umc/wegg/controller/MapRestController.java
+++ src/main/java/umc/wegg/controller/MapRestController.java
@@ -18,18 +18,18 @@ public class MapRestController {
 
     private final MapService mapService;
 
-    @PostMapping("/plans/search")
+    @GetMapping("/plans/search")
     @Operation(summary = "장소 검색(계획)", description = "계획 설정 시, 장소를 지정할때 사용하는 장소 검색 API (사용자 위치 주변 장소 리스트 반환)")
-    public ApiResponse<MapResponseDTO.SearchPlanPlaceListDTO> searchPlan(@RequestBody @Valid MapRequestDTO.SearchPlanDTO request) {
+    public ApiResponse<MapResponseDTO.SearchPlanPlaceListDTO> searchPlan(@Valid @ModelAttribute MapRequestDTO.SearchPlanDTO request) {
 
         MapResponseDTO.SearchPlanPlaceListDTO response = mapService.searchPlaceListByKeyword(request);
 
         return ApiResponse.onSuccess(response);
     }
 
-    @PostMapping("/hotplaces/search")
+    @GetMapping("/hotplaces/search")
     @Operation(summary = "장소 검색(핫플)", description = "주변 weggy 핫플 장소 검색 API (사용자 위치 주변 장소 리스트 반환)")
-    public ApiResponse<MapResponseDTO.SearchHotPlaceListDTO> searchHotPlace(@RequestBody @Valid MapRequestDTO.SearchHotPlaceDTO request) {
+    public ApiResponse<MapResponseDTO.SearchHotPlaceListDTO> searchHotPlace(@Valid @ModelAttribute MapRequestDTO.SearchHotPlaceDTO request) {
 
         MapResponseDTO.SearchHotPlaceListDTO response = mapService.searchHotPlaceListByKeyword(request);
 
